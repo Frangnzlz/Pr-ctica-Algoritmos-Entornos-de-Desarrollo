40ee4baf66d1657569aee800b44945a0316bb740
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-04-16 16:53:14 +0200

    Metodo Factorial

--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -3,8 +3,7 @@ package util;
 public abstract class Algoritmos {
 	
 	/**
-	 * 
-	 * Metodo para calcular el numero de fibonacci
+	 * Metodo para calcular el numero de fibonacci de un numero que pasamos por parametro
 	 * 
 	 * @param num Indice del numero de fibonacci que queremos
 	 * @return 
@@ -18,6 +17,19 @@ public abstract class Algoritmos {
 			actual = siguiente;
 		}
 		return actual;
+	}
+	/**
+	 * Metodo para coalcular el factorial de un numero que pasamos por parametro
+	 * 
+	 * @param num Numero que queremos calcular el factorial
+	 * @return
+	 */
+	public static int factorial(int num) {
+		int result =1;
+		for(int i = num; i > 0 ;i--) {
+			result *= i;
+		}
+		return result;		
 	}
 	
 
