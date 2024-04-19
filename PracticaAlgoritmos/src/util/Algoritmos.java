b8d1bd1cb56ca5a23ba773107395cf643787fa32
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-04-19 15:55:03 +0200

    Añadir doc

--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -6,7 +6,7 @@ public abstract class Algoritmos {
 	 * Metodo para calcular el numero de fibonacci de un numero que pasamos por parametro
 	 * 
 	 * @param num Indice del numero de fibonacci que queremos
-	 * @return 
+	 * @return devuelve el numero de fibonacci
 	 */
 	public static  int fibonacci(int num) {
 		int actual = 0, siguiente = 1, auxiliar;
@@ -22,7 +22,7 @@ public abstract class Algoritmos {
 	 * Metodo para calcular el factorial de un numero que pasamos por parametro
 	 * 
 	 * @param num Numero que queremos calcular el factorial
-	 * @return
+	 * @return Devuelve el factorial del numero introducido
 	 */
 	public static int factorial(int num) {
 		int result =1;
@@ -36,7 +36,7 @@ public abstract class Algoritmos {
 	 * Metodo para comprobar si un numero es primo o no
 	 * 
 	 * @param num Numero que queremos saber si es primo
-	 * @return
+	 * @return Devuelve un true si el numero es primo un false si no lo es
 	 */
 	public static boolean esPrimo(int num) {
 		
